52e1760321b69d024f8e745604222d4d0c32618b
wikoci <74468439+wikoci@users.noreply.github.com>
2025-04-20 20:39:41 +0000

    Create btn-actions.vue

---
+++ components/btn-actions.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <q-btn-dropdown
+      content-class="shadow-0 bg-transparent !p-1"
+      round
+      flat=""
+      dropdown-icon="bi-three-dots-vertical"
+    >
+      <div class="border-[1px] bg-zinc-50 shadow-md !rounded-lg">
+        <div class="" flat>
+          <q-list :separator="true">
+            <div v-for="item of props.options">
+              <q-item
+                dense
+                class="!py-2"
+                @click="onClick(item)"
+                v-if="item?.type !== 'separator'"
+                :clickable="true"
+              >
+                <q-item-section side>
+                  <q-icon
+                    :color="item?.iconColor || 'black'"
+                    :name="item?.icon"
+                  ></q-icon>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="action-item">
+                    {{ item?.label }}
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-separator v-if="item?.type == 'separator'" />
+            </div>
+          </q-list>
+        </div>
+      </div>
+    </q-btn-dropdown>
+  </div>
+</template>
+<script setup lang="ts">
+const emit = defineEmits(["onClick"]);
+type optionActionType = {
+  id?: string;
+  icon: string;
+  label: string;
+  iconColor?: string;
+  type?: "separator" | "item" | "header";
+};
+
+const props = defineProps<{
+  options: Array<optionActionType>;
+}>();
+
+function onClick(item: optionActionType) {
+  if (item?.type === "separator") return;
+  emit("onClick", item);
+}
+</script>
